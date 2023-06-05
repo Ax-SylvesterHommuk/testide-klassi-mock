79043574dc0aada67b49ea7fe77e12a1666f34b7
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-06-05 11:18:46 +0300

    Adder interface

---
+++ adder.ts
@@ -0,0 +1,17 @@
+interface Adder{
+    add(nr: number):void;
+    getSum():number;
+}
+
+class SimpleAdder implements Adder{
+    protected sum:number=0;
+    add(nr:number){this.sum+=nr;}
+    getSum(): number {
+        return this.sum;
+    }
+}
+
+let adder1:Adder=new SimpleAdder();
+adder1.add(3);
+adder1.add(5);
+console.log(adder1.getSum());
